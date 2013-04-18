420288f443b5136d50c24480536cf164973b7f51
Guilherme Santos <guisgb13@gmail.com>
2013-04-18 16:22:06 -0300

    Testing framework performance with 10 more silly test examples

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-import os, json, subprocess
+import os, json, time, random
 from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 def lookupAPI(domain_url):
-#	proc = subprocess.Popen(["dig", "srv", "_buddycloud-api._tcp."+domain_url], stdout=subprocess.PIPE, shell=True)
-	proc = subprocess.Popen(["cat", "requirements.txt"], stdout=subprocess.PIPE, shell=True)
-	(out, err) = proc.communicate()
+	# Look at SRV record of the given domain if the service _buddycloud-api can be found!
+	out = "executed lookupAPI test"
+	err = "none"
 	return "OUT: "+str(out)+", ERR: "+str(err)
+
+
+def testExample(domain_url):
+	# This is a temporary test example, does nothing but wait.
+	waittime = random.randint(0,10)
+	time.sleep(waittime)
+	return "OUT: waited"+str(waittime)
+
 
 #Test entries: tests to be performed by the inspector, each have a name and a function
 test_entries = {}
 test_entries['lookupAPI'] = lookupAPI
+test_entries['testExample1'] = testExample 
+test_entries['testExample2'] = testExample 
+test_entries['testExample3'] = testExample 
+test_entries['testExample4'] = testExample 
+test_entries['testExample5'] = testExample 
+test_entries['testExample6'] = testExample 
+test_entries['testExample7'] = testExample 
+test_entries['testExample8'] = testExample 
+test_entries['testExample9'] = testExample 
+test_entries['testExample10'] = testExample 
 
 
 @app.route('/')
