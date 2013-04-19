1d24162f6722ae8ce2c8648618ecd89d328f9360
Guilherme Santos <guisgb13@gmail.com>
2013-04-19 01:12:28 -0300

    Adding real API server lookup test

--- inspector.py
+++ inspector.py
@@ -19,18 +19,23 @@ def get_test_names():
 		})
 	return json.dumps(entry_names)
 
+
 @app.route('/perform_test/<test_name>/<domain_url>')
 def perform_test(test_name=None, domain_url=None):
 
 	json_return = { 'name' : test_name }
 
 	if test_name == None or test_name.strip() == "" or test_name not in test_entries:
-		json_return['output'] = "Invalid test name"
+		(exit_status, test_output) = 2, "Invalid test name"
+		json_return['exit_status'] = exit_status
+		json_return['output'] = test_output
 	else:
-		test_output = test_entries[test_name](domain_url)
+		(exit_status, test_output) = test_entries[test_name](domain_url)
+		json_return['exit_status'] = exit_status
 		json_return['output'] = test_output
 
 	return json.dumps(json_return)
+
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
--- tests.py
+++ tests.py
@@ -1,17 +1,47 @@
-import random, time
+import random, time, dns.resolver
+
+#List all test funcion definitions here, they all must receive a <domain_url> parameter.
+#The return value is a tuple (exit_status, output).
+
 
 def lookupAPI(domain_url):
 	# Look at SRV record of the given domain if the service _buddycloud-api can be found!
-	out = "executed lookupAPI test"
-	err = "none"
-	return "OUT: "+str(out)+", ERR: "+str(err)
+	answers = []
+	for answer in dns.resolver.query("_buddycloud-api._tcp."+domain_url, dns.rdatatype.SRV, raise_on_no_answer=False):
+		answers.append({
+			'domain' : answer.target.to_text()[:-1],
+			'port' : answer.port,
+			'priority' : answer.priority,
+			'weight' : answer.weight
+		})
+
+	found = "API server(s) found: "
+	if len(answers) != 0:
+		for answer in answers:
+			print "Answer found:", answer
+			found += answer['domain'] + " at " + str(answer['port'])
+		out = found
+		status = 0
+	else:
+		out = "Could not find any API servers!"
+		status = 1
+
+	return (status, out)
 
 
 def testExample(domain_url):
 	# This is a temporary test example, does nothing but wait.
 	waittime = random.randint(0,10)
 	time.sleep(waittime)
-	return "OUT: waited"+str(waittime)
+	out = "Waited "+str(waittime)
+	status = 0
+	return (status, out)
+
+
+#Then for each function,
+#add a new entry into the test_entries map below
+#with a unique name identifier as key
+#and have the respective function as value.
 
 
 #Test entries: tests to be performed by the inspector, each have a name and a function
