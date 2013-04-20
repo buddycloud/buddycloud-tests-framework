7871de3913aa9a557ce840f8cd4497062dc49ef8
Guilherme Santos <guisgb13@gmail.com>
2013-04-19 21:46:36 -0300

    Added test 0.3 (XMPP Server is up at address pointed by A record)

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
-import random, time, dns.resolver, requests
+import random, time, dns.resolver, requests, sleekxmpp
+from sleekxmpp import ClientXMPP
 
 #List all test funcion definitions here, they all must receive a multiprocessing.Queue <result> and a String <domain_url> parameter.
 #The return value is a tuple (exit_status, output). Also, right before returning, a result.put_nowait((<exit_status>, <output>)) operation must be performed.
@@ -105,6 +106,82 @@ def xmppServerAddressRecordLookup(domain_url):
 		status = 1
 	return (status, out)
 
+def xmppServerConnectionTest(domain_url):
+
+	answers = []
+	query_for_SRV_record = None
+	try:
+		query_for_SRV_record = dns.resolver.query("_xmpp-server._tcp."+domain_url, dns.rdatatype.SRV)
+	except dns.resolver.NXDOMAIN:
+		out = "No XMPP server SRV record found!"
+		status = 1
+		return (status, out)
+	except:
+		out = "A problem happened while searching for a XMPP server SRV record!"
+		status = 1
+		return (status, out)
+
+	for answer in query_for_SRV_record:
+		domain = answer.target.to_text()[:-1]
+		port = str(answer.port)
+
+		answers.append({
+			'domain' : domain,
+			'port' : port,
+			'priority' : answer.priority,
+			'weight' : answer.weight
+		})
+
+	if len(answers) != 0:
+		found = ""
+		for answer in answers:
+
+			# Check if SRV doesn't point to a CNAME record
+			try:
+				query_for_A_record = dns.resolver.query(answer['domain'], dns.rdatatype.CNAME)
+				out = "XMPP server SRV record is pointing to a CNAME record!"
+				status = 1
+				return (status, out)
+			except dns.resolver.NXDOMAIN:
+				pass
+			except dns.resolver.NoAnswer:
+				pass
+
+			# Check if SRV points to a valid A record
+			try:
+				query_for_A_record = dns.resolver.query(answer['domain'], dns.rdatatype.A)
+			except dns.resolver.NXDOMAIN:
+				out = "No XMPP server A record found!"
+				status = 1
+				return (status, out)
+			except Exception, e:
+				out = "A problem happened while searching for the XMPP server A record: "+str(e)
+				status = 1
+				return (status, ok)
+
+			# Check if there actually is a XMPP server listening on address pointed by A record
+			for record in query_for_A_record:
+				address = str(record)
+
+				xmpp_client = ClientXMPP("inspect@"+address, "")
+				if ( xmpp_client.connect(reattempt=False) ):
+					found += answer['domain'] + " at " + address + " | "
+				else:
+					out = "Could not connect with XMPP server "+answer['domain']+" at "+address
+					if found != "":
+						out += "\n | Connection succesfull to "+found
+					status = 1
+					return (status, out)
+
+		found = "Connection succesfull to XMPP server(s): "+found
+		out = found
+		status = 0
+	else:
+		out = "Could not find any XMPP server SRV records!"
+		status = 1
+	return (status, out)
+
+
 def lookupAPI(domain_url):
 	# Look at SRV record of the given domain if the service _buddycloud-api can be found!
 	answers = []
@@ -163,3 +240,4 @@ test_entries = {}
 test_entries['lookup_api'] = lookupAPI
 test_entries['xmpp_server_srv_lookup'] = xmppServerSRVLookup
 test_entries['xmpp_server_a_lookup'] = xmppServerAddressRecordLookup
+test_entries['xmpp_server_connection_test'] =  xmppServerConnectionTest
