d5b02ae19d86d756790ef47cb9456bed4702c3cf
Guilherme Santos <guisgb13@gmail.com>
2013-04-21 21:27:07 -0300

    Adding new test examples to check page layout

--- static/inspector.js
+++ static/inspector.js
@@ -65,7 +65,7 @@ function handleStartTestsLauncher(){
 // What to do in the page when a new test was issued just now
 function handleTestCreation(test_name){
 
-	$("#tests_output_table").append("<div class='input-prepend' style='width:100%;'><button id='td_"+test_name+"' class='btn disabled' style='width:25%; padding-left:5px; text-align:left;'><i id='ti_"+test_name+"' class='icon-random'></i> <span class='text-left'>"+test_name+"</span> </button><span id='to_"+test_name+"' class='test_output input uneditable-input' style='width:73%;'></span></div>");
+	$("#tests_output_table").append("<div class='input-prepend' style='width:100%;'><button id='td_"+test_name+"' class='btn disabled' style='width:25%; padding-left:5px; text-align:left;'><i id='ti_"+test_name+"' class='icon-random'></i> <span class='text-left'>"+test_name+"</span> </button><span id='to_"+test_name+"' class='test_output input uneditable-input' style='width:73%;'><small></small></span></div>");
 }
 
 function handleTestRelaunch(test_name, previous_status){
@@ -75,6 +75,7 @@ function handleTestRelaunch(test_name, previous_status){
 	$("#td_"+test_name).tooltip("destroy");
 	$("#td_"+test_name).attr("onclick", "");
 	$("#to_"+test_name).removeClass(previous_status);
+	$("#to_"+test_name).html("");
 	$("#ti_"+test_name).attr("class", "icon-random");
 }
 
@@ -91,7 +92,7 @@ function handleTestResponse(data, domain_url){
 		$("#td_"+data.name).attr("onclick", "runAgain('"+data.name+"', '"+domain_url+"', '"+getExitStatusClass(data.exit_status)+"');");
 	}
 	$("#to_"+data.name).addClass(getExitStatusClass(data.exit_status));
-	$("#to_"+data.name).text(data.output);
+	$("#to_"+data.name).html("<small>"+data.output+"</small>");
 	$("#ti_"+data.name).attr("class", getExitStatusIcon(data.exit_status) + " icon-white");
 }
 
