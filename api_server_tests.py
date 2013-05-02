083389497f93d7cbc1152f845d6441a2e6bad29f
Guilherme Santos <guisgb13@gmail.com>
2013-05-02 11:29:28 -0300

    Test output redirection bug fixed

--- api_server_tests.py
+++ api_server_tests.py
@@ -1,16 +1,26 @@
 import dns.resolver, socket
 
 def apiLookup(domain_url, redirectTestOutput=False):
-	# Look at SRV record of the given domain if the service _buddycloud-api can be found!
+	
 	answers = []
 	lookup_api_query = None
+
 	try:
 		lookup_api_query = dns.resolver.query("_buddycloud-api._tcp."+domain_url, dns.rdatatype.SRV)
+
 	except dns.resolver.NXDOMAIN:
+
+		if redirectTestOutput:
+			return []
+
 		out = "No API server record found!"
 		status = 1
 		return (status, out)
 	except:
+
+		if redirectTestOutput:
+			return []
+
 		out = "A problem happened while searching for API server record!"
 		status = 1
 		return (status, out)
@@ -29,7 +39,7 @@ def apiLookup(domain_url, redirectTestOutput=False):
 	if len(answers) != 0:
 		found = "API server record(s) found: "
 
-		if ( redirectTestOutput ):
+		if redirectTestOutput:
 			return answers
 
 		for answer in answers:
@@ -38,6 +48,10 @@ def apiLookup(domain_url, redirectTestOutput=False):
 		out = found
 		status = 0
 	else:
+
+		if redirectTestOutput:
+			return []
+		
 		out = "Could not find any API server records!"
 		status = 1
 	return (status, out)
@@ -47,12 +61,15 @@ def apiConnection(domain_url):
 	found = "API server connection was successful at: "
 
 	answers = apiLookup(domain_url, True)
+
 	for answer in answers:
 
 		try:
 			socket.create_connection((answer['domain'],answer['port']), timeout=5)
 			found += answer['domain']+":"+answer['port']+" | "
+
 		except socket.timeout:
+
 			out = "API server connection failed at "+answer['domain']+":"+str(answer['port'])
 			status = 1
 			return (status, out)
--- xmpp_server_tests.py
+++ xmpp_server_tests.py
@@ -5,18 +5,31 @@ def xmppServerServiceRecordLookup(domain_url, redirectTestOutput=False):
 
 	answers = []
 	query_for_SRV_record = None
+
 	try:
+
 		query_for_SRV_record = dns.resolver.query("_xmpp-server._tcp."+domain_url, dns.rdatatype.SRV)
+
 	except dns.resolver.NXDOMAIN:
+	
+		if redirectTestOutput:
+			return []
+		
 		out = "No XMPP server SRV record found!"
 		status = 1
 		return (status, out)
+
 	except:
+	
+		if redirectTestOutput:
+			return []
+		
 		out = "A problem happened while searching for a XMPP server SRV record!"
 		status = 1
 		return (status, out)
 
 	for answer in query_for_SRV_record:
+
 		domain = answer.target.to_text()[:-1]
 		port = str(answer.port)
 
@@ -31,13 +44,20 @@ def xmppServerServiceRecordLookup(domain_url, redirectTestOutput=False):
 		return answers
 
 	if len(answers) != 0:
+		
 		found = "XMPP server SRV record(s) found: "
+		
 		for answer in answers:
 			found += answer['domain'] + " at " + str(answer['port'])+" | "
+		
 		out = found
 		status = 0
 		return (status, out)
 	else:
+		
+		if redirectTestOutput:
+			return []
+		
 		out = "Could not find any XMPP server SRV records!"
 		status = 1
 	return (status, out)
@@ -57,9 +77,14 @@ def xmppServerAddressRecordLookup(domain_url, redirectTestOutput=False):
 			# Check if SRV doesn't point to a CNAME record
 			try:
 				query_for_A_record = dns.resolver.query(answer['domain'], dns.rdatatype.CNAME)
+
+				if redirectTestOutput:
+					return addresses
+			
 				out = "XMPP server SRV record is pointing to a CNAME record!"
 				status = 1
 				return (status, out)
+
 			except dns.resolver.NXDOMAIN:
 				pass
 			except dns.resolver.NoAnswer:
@@ -67,30 +92,47 @@ def xmppServerAddressRecordLookup(domain_url, redirectTestOutput=False):
 
 			# Check if SRV points to a valid A record
 			try:
+			
 				query_for_A_record = dns.resolver.query(answer['domain'], dns.rdatatype.A)
+
 			except dns.resolver.NXDOMAIN:
+				
+				if redirectTestOutput:
+					return addresses
+
 				out = "No XMPP server A record found!"
 				status = 1
 				return (status, out)
 			except:
+				
+				if redirectTestOutput:
+					return addresses
+				
 				out = "A problem happened while searching for the XMPP server A record!"
 				status = 1
 				return (status, ok)
 			
 			for record in query_for_A_record:
+
 				address = str(record)
 			
 				if redirectTestOutput:
 					addresses.append({'address':address, 'domain' : answer['domain']})
+
 				else:
 					found += answer['domain'] + " at " + address + " | "
 
 		if redirectTestOutput:
 			return addresses
 
+
 		out = found
 		status = 0
 	else:
+
+		if redirectTestOutput:
+			return []
+		
 		out = "Could not find any XMPP server SRV records!"
 		status = 1
 	return (status, out)
@@ -100,6 +142,7 @@ def xmppServerConnection(domain_url):
 	answers = xmppServerAddressRecordLookup(domain_url, True)
 
 	if len(answers) != 0:
+
 		found = ""
 		for answer in answers:
 
@@ -107,8 +150,11 @@ def xmppServerConnection(domain_url):
 
 			xmpp_client = ClientXMPP("inspect@buddycloud", "ei3tseq")
 			if ( xmpp_client.connect((address, 5222), reattempt=False, use_ssl=False, use_tls=False) ):
+
 				found += answer['domain'] + " at " + address + " | "
+
 			else:
+
 				out = "Could not connect with XMPP server "+answer['domain']+" at "+address
 				status = 1
 				return (status, out)
@@ -117,6 +163,7 @@ def xmppServerConnection(domain_url):
 		out = found
 		status = 0
 	else:
+		
 		out = "Could not find any XMPP server SRV records!"
 		status = 1
 	return (status, out)
