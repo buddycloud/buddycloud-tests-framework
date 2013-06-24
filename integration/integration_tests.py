0eea6eb8118e9a60c14095779c5af337aa6d503e
Guilherme Santos <guisgb13@gmail.com>
2013-06-24 17:33:58 -0300

    Source path corrected

--- integration/integration_tests.py
+++ integration/integration_tests.py
@@ -10,7 +10,7 @@ class IntegrationTest:
 
 		self.name = name
 		self.function = function
-		self.source = "github.com/buddycloud/buddycloud-tests-framework/blob/master/integration/setup/" + name + ".py"
+		self.source = "github.com/buddycloud/buddycloud-tests-framework/blob/master/integration/tests/" + name + ".py"
 
 	def jsonfy(self):
 
