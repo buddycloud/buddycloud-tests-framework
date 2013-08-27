e5f376d8766c107c21c122279d98f13eb8b4ab0f
Guilherme Santos <guisgb13@gmail.com>
2013-08-27 16:33:59 -0300

    Refactored api_server TXT record lookup test

--- installation/tests/api_server_lookup.py
+++ installation/tests/api_server_lookup.py
@@ -1,8 +1,73 @@
-import dns.resolver
+import dns.resolver, string
+from sleekxmpp import ClientXMPP
 
 #util_dependencies
 from domain_name_lookup import testFunction as domainNameLookup
+from dns_utils import getAuthoritativeNameserver
 
+
+def classifyTXTRecord(TXT_record):
+
+	if ( TXT_record.find("host=") == -1 ):
+		return {
+			'type' : 'INFO_MISSING',
+			'description' : 'HOST information missing.',
+			'record' : TXT_record
+		}
+	elif (  TXT_record.find("port=") == -1 ):
+		return {
+			'type' : 'INFO_MISSING',
+			'description' : 'PORT information missing.',
+			'record' : TXT_record
+		}
+	elif (  TXT_record.find("path=") == -1 ):
+		return {
+			'type' : 'INFO_MISSING',
+			'description' : 'PATH information missing.',
+			'record' : TXT_record
+		}
+	elif (  TXT_record.find("protocol=") == -1 ):
+		return {
+			'type' : 'INFO_MISSING',
+			'description' : 'PROTOCOL information missing.',
+			'record' : TXT_record
+		}
+
+	domain =  TXT_record[TXT_record.find("host=")+5 :  TXT_record.find("\"",  TXT_record.find("host="))]
+	port = TXT_record[TXT_record.find("port=")+5 : TXT_record.find("\"", TXT_record.find("port="))]
+	path = TXT_record[TXT_record.find("path=")+5 : TXT_record.find("\"", TXT_record.find("path="))]
+	protocol = TXT_record[TXT_record.find("protocol=")+9 : TXT_record.find("\"", TXT_record.find("protocol="))]
+
+	if ( domain == "" or port == "" or path == "" or protocol == "" ):
+		return {
+			'type' : 'MALFORMED',
+			'description' : 'Malformed TXT record.',
+			'record' : TXT_record
+		}
+
+	return {
+		'type' : 'CORRECT',
+		'domain' : domain,
+		'port' : port,
+		'path' : path,
+		'protocol' : protocol,
+		'record' : TXT_record
+	}
+
+def no_TXT_record(domain_url):
+
+	status = 1
+	briefing = "No API server TXT record found at domain <strong>"+domain_url+"</strong>!"
+	message = "We were unable to find your API server TXT record."
+	message += "<br/>Assuming the server running buddycloud will be named: <strong><em>buddycloud."
+	message += domain_url + "</em></strong>," 
+	message += "<br/>here you are a TXT record that should work:<br/>"
+	message += "<strong>_buddycloud-api._tcp." + domain_url + "\tIN TXT \"host=buddycloud."
+	message += domain_url + "\" \"protocol=https\" \"path=/api\" \"port=433\"</strong><br/>"
+	message += "<br/>Check at <a href='http://buddycloud.org/wiki/Install#buddycloud_DNS'"
+	message += "target='_blank'>http://buddycloud.org/wiki/Install#buddycloud_DNS</a>"
+	message += " for more information on how to setup the DNS for your domain."
+	return (status, briefing, message, None)
 
 def testFunction(domain_url):
 
@@ -11,79 +76,99 @@ def testFunction(domain_url):
 		return (status, briefing, message, None)
 
 	answers = []
-	lookup_api_query = None
+	query_for_TXT_record = None
 
 	try:
-		lookup_api_query = dns.resolver.query("_buddycloud-api._tcp."+domain_url, dns.rdatatype.TXT)
+
+		resolver = dns.resolver.Resolver()
+		resolver.nameservers = [ getAuthoritativeNameserver(domain_url) ]
+		query_for_TXT_record = resolver.query("_buddycloud-api._tcp."+domain_url, dns.rdatatype.TXT)
 
 	except dns.resolver.NXDOMAIN:
 
-		status = 1
-		briefing = "No API server TXT record found at domain " + domain_url + "!"
-		message = "We could not find your API server TXT record!"
-		message += "<br/>You must setup your DNS to point to the API server endpoint using a TXT record similat to the one below: "
-		message += "<br/><br/>_buddycloud-api._tcp.EXAMPLE.COM.          IN TXT \"v=1.0\" \"host=buddycloud.EXAMPLE.COM\" \"protocol=https\" \"path=/api\" \"port=443\""
-		return (status, briefing, message, None)
+		return no_TXT_record(domain_url)
 
 	except Exception, e:
 
+		if ( str(e) == "" ):
+			return no_TXT_record(domain_url)
+
 		status = 2
-		briefing = "A problem happened while searching for the API server TXT record: _buddycloud-api._tcp." + domain_url + "!"
-		message = "Something odd happened while we were searching for the API server TXT record: _buddycloud-api._tcp." + domain_url + "!"
+		briefing = "A problem happened while searching for the API server TXT record:"
+		briefing += " _buddycloud-api._tcp." + domain_url + "!"
+		message = "Something odd happened while we were searching for the API server TXT record:"
+		message += " _buddycloud-api._tcp." + domain_url + "!"
 		message += "<br/>This is the exception we got: {"+str(e)+"}"
 		message += "<br/>It is probably a temporary issue with domain " + domain_url + "."
-		message += "<br/>But it could also be a bug in our Inspector. Let us know at <email> if you think so." 
+		message += "<br/>But it could also be a bug in our Inspector."
+		message += " Let us know at <email> if you think so." 
 		return (status, briefing, message, None)
 
-	for answer in lookup_api_query:
+	classified_records = {}
 
-		answer = str(answer)
+	for answer in query_for_TXT_record:
 
-		problem = None
+		classified = classifyTXTRecord(str(answer))
 
-		if ( answer.find("host=") == -1 ):
-			problem = "API server TXT record found but it contains no API server host infomation!"
-		elif ( answer.find("port=") == -1 ):
-			problem = "API server TXT record found but it contains no API server port information!"
-		elif ( answer.find("path=") == -1 ):
-			problem = "API server TXT record found but it contains no API server path information!"
-		elif ( answer.find("protocol=") == -1 ):
-			problem = "API server TXT record found but it contains no API server protocol information!"
+		if not classified['type'] in classified_records:
+			classified_records[classified['type']] = []
+		classified_records[classified['type']].append(classified)
 
-		if ( problem != None ):
+	if len(classified_records.get('CORRECT', [])) == 0:
 
-			briefing = problem
-			status = 1
-			message = "We could find your API server TXT record! The problem is it is missing some important information."
-			message += "<br/>Please check if your API server TXT record has a format similar to this example below:"
-			message += "<br/><br/>_buddycloud-api._tcp.EXAMPLE.COM.          IN TXT \"v=1.0\" \"host=buddycloud.EXAMPLE.COM\" \"protocol=https\" \"path=/api\" \"port=443\""
-			return (status, briefing, message, None)
+		status = 1
+		briefing = "No API server TXT record found!<br/>"
+		message = briefing
 
-		domain = answer[answer.find("host=")+5 : answer.find("\"", answer.find("host="))]
-		port = answer[answer.find("port=")+5 : answer.find("\"", answer.find("port="))]
-		path = answer[answer.find("path=")+5 : answer.find("\"", answer.find("path="))]
-		protocol = answer[answer.find("protocol=")+9 : answer.find("\"", answer.find("protocol="))]
+		if ( len(classified_records.get('INFO_MISSING', [])) != 0
+		  or len(classified_records.get('MALFORMED', [])) != 0 ):
 
-		answers.append({
-			'domain' : domain,
-			'port' : port,
-			'protocol' : protocol,
-			'path' : path
-		})
+			message += "We detected you set up the following API server"
+			message += " TXT records, though.<br/>"
+			message += "They have <strong>problems:</strong>"
 
+		for record in classified_records.get('INFO_MISSING', []):
 
-	found = answers[0]['domain'] + answers[0]['path'] + ", port: " + str(answers[0]['port'])
+			message += ("<strong>%s</strong><br/><em>%s</em><br/><br/>"
+					% (record['description'], record['record']))
 
-	for i in range(1, len(answers)):
-		found = answers[i]['domain'] + answers[i]['path'] + ", port: " + str(answers[i]['port'])
+		for record in classified_records.get('MALFORMED', []):
 
-	if len(answers) == 1:
-		found = "API server record found: " + found
+			message += ("<strong>%s</strong><br/><em>%s</em><br/><br/>"
+					% (record['description'], record['record']))
+
+		return (status, briefing, message, None)
+
+	elif len(classified_records.get('CORRECT', [])) > 1:
+
+		status = 2
+		briefing = "We found multiple correct API server TXT records!"
+		message = briefing + "<br/>"
+		message += "But really, you should have just one.<br/>"
+		message += "These are the records we found: <br/>"
+
 	else:
-		found = "API server records found: " + found
 
-	briefing = found
-	status = 0
-	message = "We could succesfully find your API server TXT record on your domain!"
-	message += briefing
-	return (status, briefing, message, answers)
+		status = 0
+		briefing = "API server TXT record found: "
+		briefing += "<strong>%s</strong>" % classified_records['CORRECT'][0]['record']
+		message = "Congratulations! You have set up your API server "
+		message += "TXT record correctly.<br/>"
+
+	for record in classified_records.get('CORRECT', []):
+
+		message += ("<strong>%s://%s:%s%s</strong><br/><em>%s</em><br/><br/>"
+				% (record['protocol'],
+				   record['domain'],
+				   record['port'],
+				   record['path'],
+				   record['record']))
+
+	r = classified_records['CORRECT'][0]
+
+	return (status, briefing, message, {
+			'protocol' : r['protocol'],
+			'domain' : r['domain'],
+			'port' : r['port'],
+			'path' : r['path']
+		})
