c34ce64a984a7f08c8c82a4be2252714bf95164e
Guilherme Santos <guisgb13@gmail.com>
2013-10-03 18:15:14 -0300

    Couple more visibility tests, they aren't completely finished though

---
+++ integration/tests/visibility_test_user_channel_banned.py
@@ -0,0 +1,67 @@
+from visibility_utils import performVisibilityTests
+
+def testFunction(domain_url):
+
+	expected_results = {
+		'ALL_METADATA_ACCESS'	: {
+			True : [
+			"test_user_channel_open@" + domain_url,
+			"test_topic_channel_open@topics." + domain_url
+			],
+			False : [
+			"test_user_channel_closed@" + domain_url
+			]
+		},
+		'MOOD_STATUS_ACCESS'	: {
+			True : [
+			"test_user_channel_open@" + domain_url
+			],
+			False : [
+			"test_user_channel_closed@" + domain_url
+			]
+		},
+		'POSTS_READ_ACCESS'	: {
+			True : [
+			"test_user_channel_open@" + domain_url,
+			"test_topic_channel_open@topics." + domain_url
+			],
+			False : [
+			"test_user_channel_closed@" + domain_url
+			]
+		},
+		'SUBSCRIBERS_ACCESS'	: {
+			True : [
+			"test_user_channel_open@" + domain_url,
+			"test_topic_channel_open@topics." + domain_url
+			],
+			False : [
+			"test_user_channel_closed@" + domain_url
+			]
+		},
+		'SUBSCRIBED_TO_ACCESS'	: {
+			True : [
+			"test_user_channel_open@" + domain_url
+			],
+			False : [
+			"test_user_channel_closed@" + domain_url
+			]
+		}#,
+#		'GEOLOC_ACCESS'		: {
+#			False : [
+#			"test_user_channel_open@" + domain_url,
+#			"test_user_channel_closed@" + domain_url
+#			]
+#		}
+	}
+
+	(status, partial_report) = performVisibilityTests(domain_url, "test_user_channel_follower4", expected_results)
+
+	if status == 0:
+		briefing = "Visibility tests for <strong>test_user_channel_follower4@%s</strong> were successful!" % domain_url
+	else:
+		briefing = "Visibility tests for <strong>test_user_channel_follower4@%s</strong> were not entirely successful!" % domain_url
+
+	message = briefing + "<br/>"
+	message += partial_report
+
+	return (status, briefing, message, None)
