40400db8b150a67c5b21f2a83b19a06cc677f668
Guilherme Baptista <guisgb13@gmail.com>
2014-05-01 01:43:25 +0200

    Fixing API server TXT record lookup test: assuming correct format for the record

--- installation/tests/api_server_lookup.py
+++ installation/tests/api_server_lookup.py
@@ -1,204 +1,220 @@
 import dns.resolver, string, re
-from dns.resolver import NoAnswer, NXDOMAIN
 
 #util_dependencies
+from template_utils import bold, italic, code, code_block, breakline, link,\
+parse, render, build_output
 from domain_name_lookup import testFunction as domainNameLookup
 from dns_utils import getAuthoritativeNameserver
 
-find_version = re.compile("(\"v=[0-9]+(\.{1}[0-9]+){0,1}\"){1}")
-find_host = re.compile("(\"host=[^\"\']+\"){1}")
-find_protocol = re.compile("(\"protocol=[^\"\']+\"){1}")
-find_path = re.compile("(\"path=[^\"\']*\"){1}")
-find_port = re.compile("(\"port=[0-9]+\"){1}")
 
-check_if_is_malformed = re.compile("^( *\".*=.*\" *){5}$")
+def warning_template():
 
-def classifyTXTRecord(TXT_record):
+    briefing_template = bold("Precondition problem:") + " "
+    briefing_template += code("{{warning}}")
+    message_template = "This test did not even run. " + briefing_template
+    briefing_template = parse(briefing_template)
+    message_template = parse(message_template)
+    return briefing_template, message_template
 
-	if ( find_version.search(TXT_record) == None 
-	or find_host.search(TXT_record) == None
-	or find_protocol.search(TXT_record) == None 
-	or find_path.search(TXT_record) == None
-	or find_port.search(TXT_record) == None
-	or check_if_is_malformed.match(TXT_record) == None ):
-		return {
-			'type' : 'MALFORMED',
-			'description' : 'This TXT record is malformed.',
-			'record' : TXT_record
-		}
+def no_record_template():
 
-	domain =  TXT_record[TXT_record.find("host=")+5 :  TXT_record.find("\"",  TXT_record.find("host="))]
-        port = TXT_record[TXT_record.find("port=")+5 : TXT_record.find("\"", TXT_record.find("port="))]
-        path = TXT_record[TXT_record.find("path=")+5 : TXT_record.find("\"", TXT_record.find("path="))]
-	protocol = TXT_record[TXT_record.find("protocol=")+9 : TXT_record.find("\"", TXT_record.find("protocol="))]
+    briefing_template = "Could not find the API server " + code("TXT record")
+    briefing_template += " for " + bold("{{domain_url}}") + "!"
+    message_template = briefing_template + breakline()
+    message_template += "Please add one in your DNS settings." + breakline()
+    message_template += "Refer to the " + link("install instructions",
+        "http://buddycloud.com/install#buddycloud_dns_") + " for more info."
+    briefing_template = parse(briefing_template)
+    message_template = parse(message_template)
+    return briefing_template, message_template
 
-	if protocol != "https" and protocol != "HTTPS":
-		return {
-			'type' : 'NOT_HTTPS',
-			'description' : 'PROTOCOL must be HTTPS.',
-			'record' : TXT_record
-		}
+def record_error_template():
 
-	return {
-		'type' : 'CORRECT',
-		'domain' : domain,
-		'port' : port,
-		'path' : path,
-		'protocol' : protocol,
-		'record' : TXT_record
-	}
+    briefing_template = "We could find the API server " + code("TXT record")
+    briefing_template += " for " + bold("{{domain_url}}")
+    briefing_template += " but it is problematic!"
+    message_template = briefing_template + breakline()
+    message_template += breakline() + "This is your record:" + breakline()
+    message_template += code_block("{{&record}}")
+    message_template += "{{&error}}" + breakline()
+    message_template += "Please fix this in your DNS settings." + breakline()
+    message_template += "Refer to the " + link("install instructions",
+        "http://buddycloud.com/install#buddycloud_dns_") + " for more info."
+    briefing_template = parse(briefing_template)
+    message_template = parse(message_template)
+    return briefing_template, message_template
 
-def noTXTRecord(domain_url):
+def record_correct_template():
 
-	status = 1
-	briefing = "No correct API server TXT record found at domain <strong>"+domain_url+"</strong>!"
-	message = "We were unable to find your API server TXT record."
-	message += "<br/>Assuming the server running buddycloud will be named: <strong><em>buddycloud."
-	message += domain_url + "</em></strong>," 
-	message += "<br/>here you are a TXT record that should work:<br/>"
-	message += "<strong>_buddycloud-api._tcp." + domain_url + "\tIN TXT \"v=1.0\" \"host=buddycloud."
-	message += domain_url + "\" \"protocol=https\" \"path=/api\" \"port=433\"</strong><br/>"
-	message += "<br/>Please not that your API server TXT record won't be correct until it contains proper"
-	message += " information regarding the <em>version</em>, <em>host</em>, <em>protocol<em/>, <em>path</em> and <em>port</em>."
-	message += "<br/>Check at <a href='http://buddycloud.org/wiki/Install#buddycloud_DNS'"
-	message += "target='_blank'>http://buddycloud.org/wiki/Install#buddycloud_DNS</a>"
-	message += " for more information on how to setup the DNS for your domain."
-	return (status, briefing, message, None)
+    briefing_template = "We could find the API server " + code("TXT record")
+    briefing_template += " for " + bold("{{domain_url}}") + "!"
+    message_template = "Congratulations! " + briefing_template + breakline()
+    message_template += breakline() + "This is your record:" + breakline()
+    message_template += code_block("{{&record}}")
+    briefing_template = parse(briefing_template)
+    message_template = parse(message_template)
+    return briefing_template, message_template
+
+def warning(view):
+    return build_output(view, 2, warning_template, None)
+
+def no_record(view):
+    return build_output(view, 1, no_record_template, None)
+
+def record_error(view):
+    return build_output(view, 1, record_error_template, None)
+
+def record_correct(view):
+    return build_output(view, 0, record_correct_template, None)
+
+def _build_checker(checks):
+    def checker(TXT_record):
+        veredict = True
+        for check in checks:
+            veredict = ( veredict and
+            check[2](check[0].search(TXT_record)) == check[1] )
+        return veredict
+    return checker
+record_well_formedness_tests = []
+
+def has_version():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_version = re.compile("(v=[0-9]+(\.{1}[0-9]+){0,1}){1}"), False, is_none
+    checks.append(find_version)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_version(),
+    bold("version") + " attribute missing!"))
+
+def has_host():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_host = re.compile("(host=[^\"\']+){1}"), False, is_none
+    checks.append(find_host)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_host(),
+    bold("host") + " attribute missing!"))
+
+def has_protocol():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_protocol = re.compile("(protocol=[^\"\']+){1}"), False, is_none
+    checks.append(find_protocol)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_protocol(),
+    bold("protocol") + " attribute missing!"))
+
+def has_path():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_path = re.compile("(path=[^\"\']*){1}"), False, is_none
+    checks.append(find_path)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_path(),
+    bold("path") + " attribute missing!"))
+
+def has_port():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_port = re.compile("(port=[0-9]+){1}"), False, is_none
+    checks.append(find_port)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_port(),
+    bold("port") + " attribute missing!"))
+
+def has_exactly_five_specs():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_at_least_five = ( re.compile("^\"( *[^\"\' ]+=[^\"\' ]+ *){5}\"$"),
+        False, is_none )
+    checks.append(find_at_least_five)
+    find_at_most_five = ( re.compile("^\"( *[^\"\' ]+=[^\"\' ]+ *){6}\"$"),
+        True, is_none )
+    checks.append(find_at_most_five)
+
+    return _build_checker(checks)
+record_well_formedness_tests.insert(0, (has_exactly_five_specs(),
+"""
+A well-formed <code>TXT record</code> has exactly the following 5 attributes specified:
+<dl class='dl-horizontal'>
+    <dt>v (version)</dt>
+    <dd>the version attribute; accepts <code>floating point</code></dd>
+    <dt>host</dt>
+    <dd>the host attribute; accepts <code>string</code></dd>
+    <dt>protocol</dt>
+    <dd>the protocol attribute; accepts <code>string</code></dd>
+    <dt>path</dt>
+    <dd>the path attribute; accepts <code>string</code></dd>
+    <dt>port</dt>
+    <dd>the port attribute; accepts <code>integer</code></dd>
+</dl>
+
+Each of them being specified according to the following format: (<code>attribute=value</code>)
+<br/>
+<br/>
+There must be only whitespaces in between each attribute definition and one pair of double quotes surrounding all attribute definitions.
+<br/>
+<br/>
+For example:
+<br/>
+<div class='highlight test_log'><pre><code>"v=1.0 host=demo.buddycloud.org protocol=https path=/api port=443"</code></pre></div>"""))
 
 def testFunction(domain_url):
 
-	(status, briefing, message, output) = domainNameLookup(domain_url)
-	if ( status != 0 ):
-		return (status, briefing, message, None)
+    view = {"domain_url":domain_url}
 
-	query_for_TXT_record = None
+    if ( domainNameLookup(domain_url)[0] != 0 ):
+        view["warning"] = "%s not found!" % (domain_url)
+        return warning(view)
 
-	try:
+    try:
+        resolver = dns.resolver.Resolver()
+        resolver.nameservers = [ getAuthoritativeNameserver(domain_url) ]
+        resolver.lifetime = 5
+        answer = resolver.query("_buddycloud-api._tcp."+domain_url,
+            dns.rdatatype.TXT)
+    except Exception as e:
+        return no_record(view)
 
-		resolver = dns.resolver.Resolver()
-		resolver.nameservers = [ getAuthoritativeNameserver(domain_url) ]
-		resolver.lifetime = 5
-		query_for_TXT_record = resolver.query("_buddycloud-api._tcp."+domain_url, dns.rdatatype.TXT)
+    else:
 
-	except (NXDOMAIN, NoAnswer):
+        txt_answer = answer[0].to_text()
+        view["record"] = txt_answer
 
-		return noTXTRecord(domain_url)
+        for test in record_well_formedness_tests:
+            if ( not test[0](txt_answer) ):
+                view["error"] = code("TXT record") + " is malformed! " + test[1]
+                return record_error(view)
 
-	except Exception as e:
+        domain =  txt_answer[txt_answer.find("host=")+5:
+            txt_answer.find(" ", txt_answer.find("host=")+5)]
+        port = txt_answer[txt_answer.find("port=")+5:
+            txt_answer.find(" ", txt_answer.find("port=")+5)]
+        path = txt_answer[txt_answer.find("path=")+5:
+            txt_answer.find(" ", txt_answer.find("path=")+5)]
+        protocol = txt_answer[txt_answer.find("protocol=")+9:
+            txt_answer.find(" ", txt_answer.find("protocol=")+9)]
 
-		if ( str(e) == "" or str(e) == ("%s. does not exist." % domain_url) ):
-			return noTXTRecord(domain_url)
+        if ( protocol.lower() != "https" ):
+            view["error"] = code("TXT record") + "\'s " + bold("protocol") + "\
+                attribute must have value set to " + code("https") + "!"
+            return record_error(view)
 
-		status = 2
-		briefing = "A problem happened while searching for the API server TXT record:"
-		briefing += " <strong>_buddycloud-api._tcp." + domain_url + "</strong>!"
-		message = "Something odd happened while we were searching for the API server TXT record:"
-		message += " <strong>_buddycloud-api._tcp." + domain_url + "</strong>!"
-		message += "<br/>This is the exception we got: {"+str(e)+"}"
-		message += "<br/>It is probably a temporary issue with domain " + domain_url + "."
-		message += "<br/>But it could also be a bug in our Inspector."
-		message += " Let us know at <a href='https://github.com/buddycloud/buddycloud-tests-framework/issues'>our issue tracker</a> if you think so." 
-		return (status, briefing, message, None)
-
-	classified_records = {}
-
-	for answer in query_for_TXT_record:
-
-		classified = classifyTXTRecord(str(answer))
-
-		if not classified['type'] in classified_records:
-			classified_records[classified['type']] = []
-		classified_records[classified['type']].append(classified)
-
-	if ( len(classified_records.get('INFO_MISSING', [])) != 0
-	  or len(classified_records.get('MALFORMED', [])) != 0
-	  or len(classified_records.get('NOT_HTTPS', [])) != 0 ):
-
-		status = 1
-		briefing = "We detected incorrect API server TXT records at domain"
-		briefing += " <strong>%s</strong>." % domain_url
-		message = "We detected you set up the following incorrect API"
-		message += " server TXT records.<br/>"
-		message += "You must have just one correct API server SRV record.<br/>"
-		message += "These are the SRV records we found and their problems: <br/><br/>"
-
-		for record in classified_records.get('INFO_MISSING', []):
-
-			message += ("<strong>%s</strong><br/><em>%s</em><br/><br/>"
-					% (record['description'], record['record']))
-
-		for record in classified_records.get('MALFORMED', []):
-
-			message += ("<strong>%s</strong><br/><em>%s</em><br/><br/>"
-					% (record['description'], record['record']))
-
-		if ( len(classified_records.get('MALFORMED', [])) != 0 ):
-
-			message += "The API server TXT record must always have a <em>version</em>, <em>host</em>,"
-			message += " <em>protocol</em>, <em>path</em> and <em>port</em>.<br/>"
-			message += "<strong>Each of these properties must be defined within double quotes and separated by spaces only</strong>.<br/>"
-			message += "For example, assuming that the server running buddycloud will be named: <strong><em>buddycloud."
-			message += domain_url + "</em></strong>," 
-			message += "<br/>here you are a TXT record that should work:<br/>"
-			message += "<strong>_buddycloud-api._tcp." + domain_url + "\tIN TXT \"v=1.0\" \"host=buddycloud."
-			message += domain_url + "\" \"protocol=https\" \"path=/api\" \"port=433\"</strong><br/>"
-			message += "<br/>Please not that your API server TXT record won't be correct until it contains proper"
-			message += " information regarding the <em>version</em>, <em>host</em>, <em>protocol<em/>, <em>path</em> and <em>port</em>.<br/>"
-
-		for record in classified_records.get('NOT_HTTPS', []):
-
-			message += ("<strong>%s</strong><br/><em>%s</em><br/><br/>"
-					% (record['description'], record['record']))
-
-		if ( len(classified_records.get('NOT_HTTPS', [])) != 0 ):
-
-			message += "Please ensure your API server will run with HTTPS enabled.<br/>"
-
-		message += "See <a href='https://buddycloud.org/wiki/Install#buddycloud_DNS' target='_blank'"
-		message += " >https://buddycloud.org/wiki/Install#buddycloud_DNS</a> for more information.<br/>"
-		return (status, briefing, message, None)
-
-	elif len(classified_records.get('CORRECT', [])) == 0:
-
-		status = 1
-		briefing = "No correct API server TXT record found at "
-		briefing += "domain <strong>%s</strong>!<br/>" % domain_url
-		message = briefing + "<br/>"
-		message += "You must have one correct API server SRV record.<br/>"
-		message += "See <a href='https://buddycloud.org/wiki/Install#buddycloud_DNS' target='_blank'"
-		message += " >https://buddycloud.org/wiki/Install#buddycloud_DNS</a> for more information.<br/>"
-		return (status, briefing, message, None)
-
-	elif len(classified_records.get('CORRECT', [])) > 1:
-
-		status = 1
-		briefing = "We found multiple correct API server TXT records!"
-		message = briefing + "<br/>"
-		message += "But really, you should have just one.<br/>"
-		message += "These are the records we found: <br/><br/>"
-
-	else:
-
-		status = 0
-		briefing = "API server TXT record found: "
-		briefing += "<strong>%s</strong>" % classified_records['CORRECT'][0]['record']
-		message = "Congratulations! You have set up your API server "
-		message += "TXT record correctly.<br/><br/>"
-
-	for record in classified_records.get('CORRECT', []):
-
-		message += ("<strong>%s://%s:%s%s</strong><br/><em>%s</em><br/><br/>"
-				% (record['protocol'],
-				   record['domain'],
-				   record['port'],
-				   record['path'],
-				   record['record']))
-
-	r = classified_records['CORRECT'][0]
-
-	return (status, briefing, message, {
-			'protocol' : r['protocol'],
-			'domain' : r['domain'],
-			'port' : r['port'],
-			'path' : r['path']
-		})
+        return record_correct(view)
