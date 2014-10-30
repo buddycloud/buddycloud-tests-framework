8fd02b8d00de00bcc3a2af41aa8fd5992b0745ad
Guilherme Baptista <guisgb13@gmail.com>
2014-10-30 11:54:17 +0100

    Disco via DNS now querying for the Buddycloud server TXT record, as per XEP

--- installation/tests/buddycloud_server_disco.py
+++ installation/tests/buddycloud_server_disco.py
@@ -1,4 +1,4 @@
-import string, sleekxmpp, dns.resolver
+import string, sleekxmpp, dns.resolver, config, re
 
 #util_dependencies
 from template_utils import bold, italic, code, breakline, parse,\
@@ -6,7 +6,74 @@ render, build_output
 from domain_name_lookup import testFunction as domainNameLookup
 from dns_utils import getAuthoritativeNameserver
 
-import config
+
+def _build_checker(checks):
+    def checker(TXT_record):
+        veredict = True
+        for check in checks:
+            veredict = ( veredict and
+            check[2](check[0].search(TXT_record)) == check[1] )
+        return veredict
+    return checker
+record_well_formedness_tests = []
+
+def has_version():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_version = re.compile("(v=[0-9]+(\.{1}[0-9]+){0,1}){1}"), False, is_none
+    checks.append(find_version)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_version(),
+    bold("version") + " attribute missing!"))
+
+def has_server():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_server = re.compile("(server=[^\"\']+){1}"), False, is_none
+    checks.append(find_server)
+
+    return _build_checker(checks)
+record_well_formedness_tests.append((has_server(),
+    bold("server") + " attribute missing!"))
+
+def has_exactly_two_specs():
+
+    checks = []
+    is_none = lambda match: match == None
+
+    find_at_least_two = ( re.compile("^\"( *[^\"\' ]+=[^\"\' ]+ *){2}\"$"),
+        False, is_none )
+    checks.append(find_at_least_two)
+    find_at_most_two = ( re.compile("^\"( *[^\"\' ]+=[^\"\' ]+ *){3}\"$"),
+        True, is_none )
+    checks.append(find_at_most_two)
+
+    return _build_checker(checks)
+record_well_formedness_tests.insert(0, (has_exactly_two_specs(),
+"""
+A well-formed <code>TXT record</code> has exactly the following 2 attributes specified:
+<dl class='dl-horizontal'>
+    <dt>v (version)</dt>
+    <dd>the version attribute; accepts <code>floating point</code></dd>
+    <dt>server</dt>
+    <dd>the server attribute; accepts <code>string</code></dd>
+</dl>
+
+Each of them being specified according to the following format: (<code>attribute=value</code>)
+<br/>
+<br/>
+There must be only whitespaces in between each attribute definition and one pair of double quotes surrounding all attribute definitions.
+<br/>
+<br/>
+For example:
+<br/>
+<div class='highlight test_log'><pre><code>"v=1.0 server=server01.verona.lit."</code></pre></div>"""))
+
 
 def xmpp_connection_problem_template():
 
@@ -73,10 +140,26 @@ def buddycloud_enabled_conflict_template():
     message_template += ", we found your channel server at "
     message_template += bold("{{channel_server}}") + "!" + breakline()
     message_template += "But we also found a channel server through "
-    message_template += code("PTR record lookup") + ", which is different: "
+    message_template += code("TXT record lookup") + ", which is different: "
     message_template += bold("{{channel_server2}}") + "!" + breakline()
     message_template += "The channel server addresses found through "
-    message_template += code("Service Discovery") + " and " + code("PTR record lookup") + " must match!"
+    message_template += code("Service Discovery") + " and " + code("TXT record lookup") + " must match!"
+    briefing_template = parse(briefing_template)
+    message_template = parse(message_template)
+    return briefing_template, message_template
+
+def record_error_template():
+
+    briefing_template = "We could find the Buddycloud server " + code("TXT record")
+    briefing_template += " for " + bold("{{domain_url}}")
+    briefing_template += " but it is problematic!"
+    message_template = briefing_template + breakline()
+    message_template += breakline() + "This is your record:" + breakline()
+    message_template += code_block("{{&record}}")
+    message_template += "{{&error}}" + breakline()
+    message_template += "Please fix this in your DNS settings." + breakline()
+    message_template += "Refer to this " + link("document",
+        "http://buddycloud.github.io/buddycloud-xep/#DNS-discovery") + " for more info."
     briefing_template = parse(briefing_template)
     message_template = parse(message_template)
     return briefing_template, message_template
@@ -89,13 +172,13 @@ def is_buddycloud_enabled_template():
     message_template += "{{#discovery}}Using " + code("Service Discovery")
     message_template += ", we found your channel server at "
     message_template += bold("{{channel_server}}") + "!" + breakline()
-    message_template += "{{#ptr_record}}We also found your channel server through "
-    message_template += code("PTR record lookup") + ".{{/ptr_record}}{{/discovery}}"
-    message_template += "{{#ptr_record}}Using " + code("PTR record lookup")
+    message_template += "{{#txt_record}}We also found your channel server through "
+    message_template += code("TXT record lookup") + "." + breakline() + "{{/txt_record}}{{/discovery}}"
+    message_template += "{{#txt_record}}Using " + code("TXT record lookup")
     message_template += ", we found your channel server at "
     message_template += bold("{{channel_server}}") + "!" + breakline() + "Bu"
     message_template += "t we could not find it through "
-    message_template += code("Service Discovery") + ".{{/ptr_record}}"
+    message_template += code("Service Discovery") + ".{{/txt_record}}"
     briefing_template = parse(briefing_template)
     message_template = parse(message_template)
     return briefing_template, message_template
@@ -117,6 +200,9 @@ def warning(view):
 
 def buddycloud_enabled_with_conflict(view):
     return build_output(view, 1, buddycloud_enabled_conflict_template, None)
+
+def record_error(view):
+    return build_output(view, 1, record_error_template, None)
 
 def is_buddycloud_enabled(view):
     return build_output(view, 0, is_buddycloud_enabled_template, None)
@@ -216,16 +302,23 @@ def testFunction(domain_url):
                             resolver = dns.resolver.Resolver()
 #                            nameserver = getAuthoritativeNameserver(domain_url)
 #                            resolver.nameservers = [nameserver]
-#                            resolver.lifetime = config.DNS_TIMEOUT
-                            PTR_name = "_buddycloud-server._tcp." + domain_url
-                            answer = resolver.query(PTR_name, dns.rdatatype.PTR)
+                            TXT_name = "_bcloud-server._tcp." + domain_url
+                            answer = resolver.query(TXT_name, dns.rdatatype.TXT)
                         except Exception:
                             pass
                         else:
-                            view["ptr_record"] = True
-                            ptr_answer = answer[0].target.to_text()[:-1]
-                            if ( item_jid != ptr_answer ):
-                                view["channel_server2"] = ptr_answer
+                            view["txt_record"] = True
+                            txt_answer = answer[0].to_text()
+
+                            for test in record_well_formedness_tests:
+                                if ( not test[0](txt_answer) ):
+                                    view["error"] = code("TXT record") + " is malformed! " + test[1]
+                                    return record_error(view)
+
+                            answer_jid =  txt_answer[txt_answer.find("server=")+7:txt_answer.find(" ", txt_answer.find("server=")+7)]
+
+                            if ( item_jid != answer_jid ):
+                                view["channel_server2"] = answer_jid
                                 return buddycloud_enabled_with_conflict(view)
 
                         return is_buddycloud_enabled(view)
@@ -243,13 +336,20 @@ def testFunction(domain_url):
         resolver = dns.resolver.Resolver()
 #        nameserver = getAuthoritativeNameserver(domain_url)
 #        resolver.nameservers = [nameserver]
-#        resolver.lifetime = config.DNS_TIMEOUT
-        PTR_name = "_buddycloud-server._tcp." + domain_url
-        answer = resolver.query(PTR_name, dns.rdatatype.PTR)
+        TXT_name = "_bcloud-server._tcp." + domain_url
+        answer = resolver.query(TXT_name, dns.rdatatype.TXT)
     except Exception:
         return disco_situation() 
     else:
-        view["ptr_record"] = True
-        ptr_answer = answer[0].target.to_text()[:-1]
-        view["channel_server"] = ptr_answer
+        view["txt_record"] = True
+        txt_answer = answer[0].to_text()
+
+        for test in record_well_formedness_tests:
+            if ( not test[0](txt_answer) ):
+                view["error"] = code("TXT record") + " is malformed! " + test[1]
+                return record_error(view)
+
+        answer_jid =  txt_answer[txt_answer.find("server=")+7:txt_answer.find(" ", txt_answer.find("server=")+7)]
+
+        view["channel_server"] = answer_jid
         return is_buddycloud_enabled(view)
